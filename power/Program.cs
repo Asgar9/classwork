74647ba597c6658a889b61c321d3c4356660ba3a
Asgar Y. Asgarov <asgaryas@code.local>
2022-08-03 21:46:17 +0400

    test

---
+++ power/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(print("filan","filankesov","filani"));
+            //Console.WriteLine(power(5));
+        }
+        //static int power(int num1) 
+        //{
+        //    return num1*num1;
+        //}
+        static string print(string name)
+        {
+            return name;
+
+        }
+        static string print(string name, string surname)
+        {
+            return name +" " + surname;
+        }
+        static string print(string name, string surname, string fname) 
+        {
+            return name + " " + fname[0] +"."+ surname;
+        }
+    }
+}
